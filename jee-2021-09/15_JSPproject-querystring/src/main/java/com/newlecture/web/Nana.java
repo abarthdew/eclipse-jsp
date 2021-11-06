4461a4fa5bd5125e62fe3e201ebfca02c090a7fd
nicky0419@daum.net <scum-hasser0692>
2021-11-06 12:05:25 +0900

    15~20

---
+++ jee-2021-09/15_JSPproject-querystring/src/main/java/com/newlecture/web/Nana.java
@@ -0,0 +1,28 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/querystring")
+public class Nana extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = res.getWriter();
+		
+		int cnt = Integer.parseInt(req.getParameter("cnt"));
+		// req.getParameter("cnt")는 무조건 문자열로 전달됨
+		
+		for (int i=0; i<cnt; i++) {
+			out.println(i + " : 안녕 Servlet !! </br>");
+		}
+	}
+}
