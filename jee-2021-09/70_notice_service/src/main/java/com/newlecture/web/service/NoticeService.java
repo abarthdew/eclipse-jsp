69e273f4470901c2dd49fb9eafdb20cdf1c03081
Jonah <vsevolod117@gmail.com>
2021-11-24 23:33:33 +0900

    ~74

--- jee-2021-09/70_notice_service/src/main/java/com/newlecture/web/service/NoticeService.java
+++ jee-2021-09/70_notice_service/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -33,14 +33,22 @@ public class NoticeService {
 	}
 	
 	public Notice getNotice(int id) {
+		String sql = "SELECT * FROM NOTICE WHERE ID = ?";
 		return null;
 	}
 	
 	public Notice getNextNotice(int id) {
+		String sql = "SELECT * FROM NOTICE"
+				+ "    WHERE ID = (SELECT ID FROM NOTICE WHERE REGDATE > ("
+				+ "            SELECT REGDATE FROM NOTICE WHERE ID = 3)"
+				+ "            AND ROWNUM = 1"
+				+ "    )";
 		return null;
 	}
 	
 	public Notice getPrevNotice(int id) {
+		String sql = "SELECT * FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC)"
+				+ "    WHERE REGDATE < (SELECT REGDATE FROM NOTICE WHERE ID = 3) AND ROWNUM = 1";
 		return null;
 	}
 	
