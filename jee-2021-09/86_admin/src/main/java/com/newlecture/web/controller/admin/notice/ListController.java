15449935dd18c7b4cd3698c99cca45594ea426f4
Jonah <vsevolod117@gmail.com>
2021-12-03 23:41:29 +0900

    89

--- jee-2021-09/86_admin/src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
+++ jee-2021-09/86_admin/src/main/java/com/newlecture/web/controller/admin/notice/ListController.java
@@ -14,6 +14,18 @@ import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/admin/notice/list")
 public class ListController extends HttpServlet{
+	// 404 : url이 없어 발생하는 오류(url 오류)
+	// 405 : url은 있는데 그 안에 받을 수 있는 메서드가 없는 경우(메서드 오류)
+	// 403 : url, 메서드는 있는데 권한이 없는 경우(보안 오류)
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] openIds = request.getParameterValues("open-id"); // 여러 개 값이므로 배열로 받음
+		String[] delIds = request.getParameterValues("del-id");
+		
+		for(String openId : openIds) System.out.printf("open ID : %s\n", openId);
+		for(String delId : delIds) System.out.printf("del ID : %s\n", delId);
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
